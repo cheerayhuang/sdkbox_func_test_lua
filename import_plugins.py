95e1b948983c06fa92f7d974e4fac5ff054942bf
cheeray.huang <cheeray.huang@gmail.com>
2015-09-16 16:12:05 +0800

    Update import_plugins.py, handling opts.

--- import_plugins.py
+++ import_plugins.py
@@ -5,6 +5,7 @@ import sys
 import subprocess
 import re
 import traceback
+import getopt
 
 def trim_folder(f):
     return os.path.abspath(f).rstrip('/')
@@ -32,7 +33,26 @@ def run_proj():
     return 0
 
 def print_usage():
-    print '''Usage: import_plugins.py <plugins_folder>'''
+    print '''
+Usage: import_plugins.py <plugins_folder> [--dont_run]
+NOTE: plugins_folder should be followed by the command as the 1st argument.
+    '''
+
+def get_options(argv, options, long_opts):
+    try:
+        opts, args = getopt.getopt(argv, options, long_opts)
+    except getopt.GetoptError:
+        print "some options are invalid."
+        print_usage()
+
+    params = {}
+    params['dont_run'] = False
+
+    for opt, arg in opts:
+        if opt == '--dont_run':
+            params['dont_run'] = True
+
+    return params
 
 def main(argv):
     if len(argv) == 0:
@@ -41,11 +61,17 @@ def main(argv):
 
     plugins_dir = trim_folder(argv[0])
     if not os.path.isdir(plugins_dir):
+        print '# ERROR: "{0}" is a invalid directory.'.format(argv[0])
+        print_usage()
         return 1
 
+    params = get_options(argv[1:],  '', ['dont_run'])
 
     if import_plugins(plugins_dir) != 0:
         return 1
+
+    if params['dont_run']:
+        return 0
 
     if run_proj() != 0:
         return 1
